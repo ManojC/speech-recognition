43e568d9a89090c8d53d91a812fdeac8cbe55773
Manoj Chalode <chalodem@gmail.com>
2017-12-30 22:36:13 +0000

    master - added support for diff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from "angularfire2/auth";
 import * as firebase from 'firebase'
+import  * as Diff from "text-diff";
+
 import { AngularFireDatabase } from 'angularfire2/database';
 import { SpeechRecognition } from "./speech-recognition";
+
 
 @Component({
     selector: 'app-root',
@@ -14,15 +17,22 @@ export class AppComponent implements OnInit {
     private _SpeechRecognition: SpeechRecognition;
     private dbList: any;
     private user: any;
+    private diff: Diff;
+    private isStopped: boolean = true;
+
+    private time: string;
+    private resetTimer: any;
+
     private window: any = window;
     private textBody: HTMLElement;
     private finalResults: string = "";
     
     constructor(private _AngularFireAuth: AngularFireAuth,
-        private _db: AngularFireDatabase) { }
+        private _db: AngularFireDatabase) { 
+        this.diff = new Diff();            
+    }
 
     public ngOnInit(): void {
-        this.initialise();
         this._AngularFireAuth.authState.subscribe((user: firebase.User) => {
             if (user) {
                 this.updateStorage(user);
@@ -32,13 +42,19 @@ export class AppComponent implements OnInit {
     }
 
     private initialise(): void {
+        if (this._SpeechRecognition) {
+            return;
+        }
         this.textBody = document.getElementById('text');
-        this._SpeechRecognition = new SpeechRecognition('en-Uk', true, true, 1);
+        this._SpeechRecognition = this._SpeechRecognition || new SpeechRecognition('en-Uk', true, true, 1);
         this._SpeechRecognition.onresult = this.onResult.bind(this);
         this._SpeechRecognition.start();
     }
 
     private onResult(event: any): void {
+        if (this.isStopped) {
+            return;
+        }
         var intermidiateResults = '';
         for (var i = event.resultIndex; i < event.results.length; i++) {
             var transcript = event.results[i][0].transcript;
@@ -103,5 +119,25 @@ export class AppComponent implements OnInit {
                 console.log(response);
             });
         }
+    }
+
+    private timer() {
+        this.isStopped = !this.isStopped;
+        if (this.isStopped) {
+            if (this.resetTimer) {
+                clearInterval(this.resetTimer);
+                var textDiff = this.diff.main(document.getElementById('text-sample').innerText, document.getElementById('text').innerText);
+                document.getElementById('result').innerHTML = this.diff.prettyHtml(textDiff);
+            }
+            return;
+        }
+        this.initialise();
+        let time: number = 0;
+        this.resetTimer = setInterval(() => {
+            ++time;
+            let minutes: number = Math.floor(time / 60);
+            let seconds: number = time % 60;
+            this.time = `${minutes < 10 ? `0${minutes}` : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`;
+        }, 1000);
     }
 }
