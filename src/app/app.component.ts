a45e8de72985ff3135dd871b293524f3846219c3
Manoj Chalode <chalodem@gmail.com>
2017-12-30 20:36:12 +0000

    master added firebase support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from "angularfire2/auth";
+import * as firebase from 'firebase'
+import { AngularFireDatabase } from 'angularfire2/database';
 import { SpeechRecognition } from "./speech-recognition";
 
 @Component({
@@ -9,16 +12,28 @@ import { SpeechRecognition } from "./speech-recognition";
 export class AppComponent implements OnInit {
 
     private _SpeechRecognition: SpeechRecognition;
+    private dbList: any;
+    private user: any;
+    private window: any = window;
     private textBody: HTMLElement;
     private finalResults: string = "";
+    
+    constructor(private _AngularFireAuth: AngularFireAuth,
+        private _db: AngularFireDatabase) { }
 
     public ngOnInit(): void {
         this.initialise();
+        this._AngularFireAuth.authState.subscribe((user: firebase.User) => {
+            if (user) {
+                this.updateStorage(user);
+                this.initialiseDatabase();
+            }
+        });
     }
 
     private initialise(): void {
         this.textBody = document.getElementById('text');
-        this._SpeechRecognition = new SpeechRecognition('en-Uk',  true, true, 1);
+        this._SpeechRecognition = new SpeechRecognition('en-Uk', true, true, 1);
         this._SpeechRecognition.onresult = this.onResult.bind(this);
         this._SpeechRecognition.start();
     }
@@ -34,6 +49,59 @@ export class AppComponent implements OnInit {
                 intermidiateResults += transcript;
             }
         }
-        this.textBody.innerHTML = this.finalResults + '<span style="color:blue">' + intermidiateResults + '</span>';
+        this.finalResults = this.translatePanctuations(this.finalResults);
+        intermidiateResults = this.translatePanctuations(intermidiateResults);
+        this.textBody.innerHTML = this.finalResults + '<span style="color:#777">' + intermidiateResults + '</span>';
+    }
+
+    private translatePanctuations(message: string): string {
+        message = message.replace(/full stop/ig, '.');
+        message = message.replace(/comma/ig, ',');
+        message = message.replace(/exclamation/ig, '!');
+        message = message.replace(/question mark/ig, '?');
+
+        message = message.replace(' .', '. ');
+        message = message.replace(' ,', ', ');
+        message = message.replace(' !', '! ');
+        message = message.replace(' ?', '? ');
+
+        message = message.replace('.  ', '. ');
+        message = message.replace(',  ', ', ');
+        message = message.replace('!  ', '! ');
+        message = message.replace('?  ', '? ');
+
+        return message.trim();
+    }
+
+    private logout(): void {
+        this._AngularFireAuth.auth.signOut().then((response: any) => {
+            this.user = null;
+            sessionStorage.removeItem('user');
+        });
+    }
+
+    private login(): void {
+        this._AngularFireAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    }
+
+    private updateStorage(user: any): void {
+        this.user = {
+            displayName: user.displayName,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            photoURL: user.photoURL,
+            providerId: user.providerId,
+            uid: user.uid
+        };
+        sessionStorage.setItem('user', JSON.stringify(this.user));
+    }
+
+    private initialiseDatabase(): void {
+        if (this.user && this.user.uid) {
+            this.dbList = this._db.list(`/${this.user.uid}/speech`);
+            this.dbList.subscribe((response: any[]) => {
+                console.log(response);
+            });
+        }
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { CommonModule } from "@angular/common";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { environment } from "../environments/environment";
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        CommonModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule,
+        RouterModule.forRoot([
+            {
+                path: '**',
+                component: AppComponent
+            }
+        ])
+    ],
+    providers: [],
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule { }
